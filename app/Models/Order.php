931ea9c1085d2b6d49aacba6122a298712c3847d
harryarcia205-lab <harryarcia205@gmail.com>
2026-07-24 19:36:35 -0600

    Actaulizar models Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class order extends Model
+class Order extends Model
 {
     protected $fillable=[
         'client_id',
