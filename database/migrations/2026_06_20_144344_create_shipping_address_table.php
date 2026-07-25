22f7080d1c64a176859730906526cb1c2bd73436
harryarcia205-lab <harryarcia205@gmail.com>
2026-07-25 11:08:11 -0600

    Actualizar migrations shippingaddress

--- database/migrations/2026_06_20_144344_create_shipping_address_table.php
+++ database/migrations/2026_06_20_144344_create_shipping_address_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string("neighborhood", 255);
             $table->string("city", 255);
             $table->string("reference_location", 25);
-            $table->string("state_address", 25);  
+            $table->string("state_address", 25);
             $table->timestamps();
         });
     }
