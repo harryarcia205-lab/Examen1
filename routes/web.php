0117b25b71b8561aa17b5482e6f003cc465d38b4
harryarcia205-lab <harryarcia205@gmail.com>
2026-07-29 17:27:42 -0600

    Actualizar routes web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ShippingAddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('clients', ClientController::class);
+    Route::resource('shippingAddress', ShippingAddressController::class);
 });
 
 require __DIR__.'/auth.php';
