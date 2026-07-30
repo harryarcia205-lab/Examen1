af833fc1dbc2b8e64632437b6a4bf284a931e9ea
harryarcia205-lab <harryarcia205@gmail.com>
2026-07-29 18:18:42 -0600

    Actualizar routes web

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\OrderlinesController;
 use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\FactoryArticlesController;
 use App\Http\Controllers\FactoriesController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -29,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('articles', ArticlesController::class);
     Route::resource('factoryarticles', FactoryArticlesController::class);
     Route::resource('factories', FactoriesController::class);
+    Route::resource('note',NoteController::class);
     {
         
     }
