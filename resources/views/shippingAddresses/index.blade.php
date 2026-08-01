eed2bfe55966b4375eadc1bdbb11bf0e111cd003
harryarcia205-lab <harryarcia205@gmail.com>
2026-08-01 01:14:01 -0600

    Actualizar views shippingAddresses index

--- resources/views/shippingAddresses/index.blade.php
+++ resources/views/shippingAddresses/index.blade.php
@@ -52,13 +52,13 @@
                                     </td>
                                     <td class="px-6 py-4 text-right">
                                         <div class="flex justify-end items-center space-x-3 opacity-0 group-hover:opacity-100 transition-opacity">
-                                            <a href="{{ route('shipping_addresses.show', $shipping_address) }}" class="text-gray-400 hover:text-indigo-600 dark:hover:text-indigo-400 transition-colors" title="Ver">
+                                            <a href="{{ route('shipping_addresses.show', $shipping_addresses) }}" class="text-gray-400 hover:text-indigo-600 dark:hover:text-indigo-400 transition-colors" title="Ver">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
                                             </a>
                                             <a href="{{ route('shipping_addresses.edit', $shipping_addresses) }}" class="text-gray-400 hover:text-amber-500 transition-colors" title="Editar">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                                             </a>
-                                            <form action="{{ route('shipping_address.destroy', $shipping_addresses) }}" method="POST" class="inline" id="form-delete-{{ $shipping_address->id }}">
+                                            <form action="{{ route('shipping_addresses.destroy', $shipping_addresses) }}" method="POST" class="inline" id="form-delete-{{ $shipping_address->id }}">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="button" onclick="confirmarEliminacion({{ $shipping_addresses->id }})" class="text-gray-400 hover:text-red-500 transition-colors" title="Eliminar">
