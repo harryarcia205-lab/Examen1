a52708ca3176cfc1d3a19af6bb790cd59ac9f69e
harryarcia205-lab <harryarcia205@gmail.com>
2026-08-02 21:43:06 -0600

    Actualizar Requests ShippingAddress

--- app/Http/Requests/ShippingAddress.php
+++ app/Http/Requests/ShippingAddress.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class AddressShippingRequest extends FormRequest
+class ShippingAddressRequest extends FormRequest
 {
     public function authorize(): bool
     {
