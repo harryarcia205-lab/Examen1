7f33da95086008f059818a507c50901fba79746b
harryarcia205-lab <harryarcia205@gmail.com>
2026-08-05 15:43:21 -0600

    Actualizar Requests Article

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -1,27 +1,65 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Requests;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
 
-class Article extends Model
+class ArticleRequest extends FormRequest
 {
-    protected $fillable=[
-        "code_internal",
-        "description",
-        "price",
-        "cost",
-        "state",
-        "date_record"
-    ];
-
-    public function order_lines()
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
     {
-        return $this->hasMany(Order_line::class);
+        return true;
     }
 
-    public function factory_articles()
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
     {
-        return $this->hasMany(Factory_article::class);
+        return [
+            'code_internal'=>"string|required||min:4|max:20",
+            'description'=>"string|required||min:3|max:50",
+            'price'=>"required",
+            'cost'=>"required",
+            'state'=>"string|required||min:3|max:30",
+            'date_record'=>"date|required",
+        ];
+    }
+
+    public function messages():array
+    {
+        return[
+            'code_internal.string'=>'El campo solo permite caracteres',
+            'code_internal.required'=>'El campo es requerido',
+            'code_internal.min'=>'El minimo de caractesres es 4',
+            'code_internal.max'=>'El maximo de caracteres es 20',
+
+            'description.string'=>'La descripcion solo permite caracteres',
+            'description.required'=>'El campo es requerido',
+            'min.integer'=>'El minimo de caractesres es 3',
+            'max.integer'=>'El maximo de caracteres es 50',
+
+           
+            'price.required'=>'El campo es requerido',
+
+           
+            'cost.required'=>'El campo es requerido',
+
+            'state.string'=>'El estado solo permite caracteres',
+            'state.required'=>'El campo es requerido',
+            'state.min'=>'El minimo de caractesres es 3',
+            'state.max'=>'El maximo de caracteres es 30',
+
+            'date_record.date'=>'El campo solo permite fecha de ingreso',
+            'date_record.required'=>'El campo es requerido',
+            
+
+        ];
     }
 }
