a6a7a71b49e18cbba04704f4897bc46c6cb73bc1
harryarcia205-lab <harryarcia205@gmail.com>
2026-08-12 21:46:20 -0600

    Actualizar Controllers Address_shippingController

--- app/Http/Controllers/Address_shippingController.php
+++ app/Http/Controllers/Address_shippingController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Address_shipping;
-use App\Http\Requests\Address_shippingRequest;  
+use App\Http\Requests\Address_shippingRequest;
 use App\Models\Customer;
 use App\Models\Order_line;
 use illuminate\Http\RedirectResponse;
